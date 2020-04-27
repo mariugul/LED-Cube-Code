15833a6e19592ce6b829af3b8b1c99678a0277c8
C. K <32439744+mariugul@users.noreply.github.com>
2020-04-27 04:44:45 +0200

    Create timer.h

---
+++ ArduinoUNO/inc/timer.h
@@ -0,0 +1,26 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                     timer.h                           *
+ *                                                       *
+ * This file initializes the timer for use of interrupts *
+ *                                                       *
+ *                                                       *
+ * By: Marius C. K. Gulbrandsen                          *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+// Header Guard
+//---------------------------------
+#ifndef __TIMER_H__
+#define __TIMER_H__
+
+// Includes
+//---------------------------------
+#include <avr/io.h>
+
+// Defines
+//---------------------------------
+
+// Function Prototypes
+//---------------------------------
+void timer_init();
+
+#endif
