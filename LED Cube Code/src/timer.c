940b3045fee6afd67a9494ae761e7a79aeb2e2ff
C. K <32439744+mariugul@users.noreply.github.com>
2020-06-20 14:30:29 +0200

    Update timer.c

--- LED Cube Code/src/timer.c
+++ LED Cube Code/src/timer.c
@@ -18,18 +18,22 @@
 //---------------------------------
 void timer_init()
 {
-	//OCR1A = 0x3D08;
-	OCR1A = 156; // 10ms with 1024 prescaler
-
-	// Mode 4, CTC on OCR1A
-	TCCR1B |= (1 << WGM12);
-
-	//Set interrupt on compare match
-	TIMSK1 |= (1 << OCIE1A);
+	// Set up timer interrupt
+	OCR1A = 39;						     // 39 gives 2.5ms interrupts with 1024 prescaler = 10ms patterns and 50Hz
+	TCCR1B |= (1 << WGM12);				 // Mode 4, CTC on OCR1A
+	TIMSK1 |= (1 << OCIE1A);			 // Set interrupt on compare match
+	TCCR1B |= (1 << CS12) | (1 << CS10); // Set prescaler to 1024 and start the timer
 	
-	// Set prescaler to 1024 and start the timer
-	TCCR1B |= (1 << CS12) | (1 << CS10);
-	
-	// Global interrupt enable
+}
+
+void global_interrupt_enable()
+{
+	// Enable Global Interrupts
 	sei();
+}
+
+void global_interrupt_disable()
+{
+	// Disable Global Interrupts
+	cli();
 }
