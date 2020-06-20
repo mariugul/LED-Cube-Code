95e4db46291f795de7fb2f36cec41e1662c23a0d
C. K <32439744+mariugul@users.noreply.github.com>
2020-06-20 14:30:18 +0200

    Update gpio.h

--- LED Cube Code/inc/gpio.h
+++ LED Cube Code/inc/gpio.h
@@ -16,67 +16,37 @@
 //---------------------------------
 #include <avr/io.h>
 
-// Typedefs
-//---------------------------------
-enum {PLANE1, PLANE2, PLANE3, PLANE4};
+// Defines
+// -------------------------------------------
+// Bitmask for extracting the correct part of the pattern
+#define PORT_B_MASK 0x3F00
+#define PORT_C_MASK 0xC000
+#define PORT_D_MASK 0x00FF
 
-// Macros for setting pins
-//---------------------------------
-#define SET(PORTX, N) (PORTX |=  (1 << N)) // Set a pin HIGH
-#define CLR(PORTX, N) (PORTX &= ~(1 << N)) // Clear a pin to LOW
+// Shifts correct values from pattern buffer to port-variables
+#define SHIFT_PORT_B 8
+#define SHIFT_PORT_C 10
 
-// Decodes what port the pin belongs to and sets it
-#define SET_LED(IDX, PIN)										\
-	({															\
-		if      (IDX >= 0 && IDX <= 7)  PORTD |= (1 << PIN);	\
-		else if (IDX > 7  && IDX <= 13) PORTB |= (1 << PIN);	\
-		else if (IDX > 13 && IDX <= 15)	PORTC |= (1 << PIN);	\
-	})
-			
-// Connect plane to ground
-#define SET_PLANE(PLANE)								\
-({														\
-	if      (PLANE == PLANE1)  PORTC |= (1 << PINA0);   \
-	else if (PLANE == PLANE2)  PORTC |= (1 << PINA1);   \
-	else if (PLANE == PLANE3)  PORTC |= (1 << PINA2);	\
-	else if (PLANE == PLANE4)  PORTC |= (1 << PINA3);	\
-})
+// Bitmask for the correct plane IO output
+#define PLANE1_MASK 0x01
+#define PLANE2_MASK 0x02
+#define PLANE3_MASK 0x04
+#define PLANE4_MASK 0x08
 
+// Number of planes in cube
+#define NR_OF_PLANES 4
 
-// Define pins to Arduino numbering
-//---------------------------------
-#define LEDPIN 5 // Test LED speed 
+// Base Frequency of LED cube
+#define FREQUENCY 50
 
-// PORTB 
-#define PIN13 5
-#define PIN12 4
-#define PIN11 3
-#define PIN10 2
-#define PIN9  1
-#define PIN8  0
+// Minimum possible time variable
+#define MIN_PATTERN_TIME 10
 
-// PORTD
-#define PIN7 7
-#define PIN6 6
-#define PIN5 5
-#define PIN4 4
-#define PIN3 3
-#define PIN2 2
-#define PIN1 1
-#define PIN0 0
+// Index of pattern table buffer time variable
+#define TIME_IDX 4
 
-// PORTC
-#define PINA5 5
-#define PINA4 4
-#define PINA3 3
-#define PINA2 2
-#define PINA1 1
-#define PINA0 0
-
-// Define PORT indexes for knowing which port the pin numbers relate to
-//---------------------------------
-#define PORTD_IDX 7  // The first 7 pins belong to PORTD
-#define PORTB_IDX 13 // The next 6 pins belong to PORTB
-#define PORTC_IDX 15 // The next 2 pins belong to PORTC
+// Function Prototypes
+// -------------------------------------------
+void gpio_init();
 
 #endif
