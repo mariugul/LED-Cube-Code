8cfc440daddb59bf0afb820a0f19813a5e368d24
Marius C. K <marius_gulbrandsen@hotmail.no>
2020-08-25 14:41:47 +0200

    Defaulted pattern.h with blinking the LED cube.

--- LED Cube Code/inc/pattern.h
+++ LED Cube Code/inc/pattern.h
@@ -6,8 +6,9 @@
 // Pattern that LED cube will display
 //---------------------------------
 const PROGMEM uint16_t pattern_table[] = {
+	// Blink all LEDs every 250ms
 	0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 250,
-	//0x0000, 0x0000, 0x0000, 0x0000, 250,
+	0x0000, 0x0000, 0x0000, 0x0000, 250,
 };
 
 
